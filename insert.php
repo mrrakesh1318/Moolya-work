523fb4d416289f0544aff6866369fd979440929f
mrrakesh1318 <mrrakeshgupta1318@gmail.com>
2021-05-09 17:52:31 +0530

    Added remote database

--- insert.php
+++ insert.php
@@ -1,22 +1,22 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "123456";
+$servername = "remotemysql.com";
+$username = "xTU08uuu2F";
+$password = "OWj6leo64z";
 
-try {
-  $conn = new PDO("mysql:host=$servername", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "CREATE DATABASE IF NOT EXISTS myDB";
-  // use exec() because no results are returned
-  $conn->exec($sql);
-  echo "Database created successfully<br>";
-} catch(PDOException $e) {
-  echo $sql . "<br>" . $e->getMessage();
-}
+// try {
+//   $conn = new PDO("mysql:host=$servername", $username, $password);
+//   // set the PDO error mode to exception
+//   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//   $sql = "CREATE DATABASE IF NOT EXISTS xTU08uuu2F";
+//   // use exec() because no results are returned
+//   $conn->exec($sql);
+//   echo "Database created successfully<br>";
+// } catch(PDOException $e) {
+//   echo $sql . "<br>" . $e->getMessage();
+// }
 
 //Created DB
-$dbname ="myDB";
+$dbname ="xTU08uuu2F";
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
